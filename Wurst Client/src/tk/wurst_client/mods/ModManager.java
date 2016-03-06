acf06de94ffc86e925507d4e02d5edb2fa9e4486
Alexander01998 <contact.wurstimperium@gmail.com>
2016-03-06 21:18:35 +0100

    :dart: Add Trajectories

--- Wurst Client/src/tk/wurst_client/mods/ModManager.java
+++ Wurst Client/src/tk/wurst_client/mods/ModManager.java
@@ -136,6 +136,7 @@ public class ModManager
 	public final TiredMod tiredMod = new TiredMod();
 	public final TracersMod tracersMod = new TracersMod();
 	public final TpAuraMod tpAuraMod = new TpAuraMod();
+	public final TrajectoriesMod trajectoriesMod = new TrajectoriesMod();
 	public final TriggerBotMod triggerBotMod = new TriggerBotMod();
 	public final TrollPotionMod trollPotionMod = new TrollPotionMod();
 	public final TrueSightMod trueSightMod = new TrueSightMod();
