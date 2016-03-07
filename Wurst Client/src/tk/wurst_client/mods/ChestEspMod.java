6b3e22fba66d7de66b59ddadb522a418ef100e16
Alexander01998 <contact.wurstimperium@gmail.com>
2016-03-07 21:21:41 +0100

    :rocket: Fix ChestESP causing lag spikes on slow computers
    This was happening because ChestESP was searching for piston crates,
    which no longer exist since Minecraft 1.8.

--- Wurst Client/src/tk/wurst_client/mods/ChestEspMod.java
+++ Wurst Client/src/tk/wurst_client/mods/ChestEspMod.java
@@ -7,31 +7,22 @@
  */
 package tk.wurst_client.mods;
 
-import java.util.ArrayList;
-
-import net.minecraft.block.Block;
-import net.minecraft.block.state.IBlockState;
 import net.minecraft.entity.item.EntityMinecartChest;
 import net.minecraft.tileentity.TileEntityChest;
 import net.minecraft.tileentity.TileEntityEnderChest;
-import net.minecraft.util.BlockPos;
 import tk.wurst_client.events.listeners.RenderListener;
-import tk.wurst_client.events.listeners.UpdateListener;
 import tk.wurst_client.mods.Mod.Category;
 import tk.wurst_client.mods.Mod.Info;
 import tk.wurst_client.navigator.NavigatorItem;
 import tk.wurst_client.utils.RenderUtils;
 
 @Info(category = Category.RENDER,
-	description = "Allows you to see chests through walls.\n"
-		+ "Tip: This works with the piston crates on HiveMC.",
+	description = "Allows you to see chests through walls.",
 	name = "ChestESP")
-public class ChestEspMod extends Mod implements UpdateListener, RenderListener
+public class ChestEspMod extends Mod implements RenderListener
 {
-	private int range = 50;
 	private int maxChests = 1000;
 	public boolean shouldInform = true;
-	private ArrayList<BlockPos> matchingBlocks = new ArrayList<BlockPos>();
 	
 	@Override
 	public NavigatorItem[] getSeeAlso()
@@ -44,7 +35,6 @@ public class ChestEspMod extends Mod implements UpdateListener, RenderListener
 	public void onEnable()
 	{
 		shouldInform = true;
-		wurst.events.add(UpdateListener.class, this);
 		wurst.events.add(RenderListener.class, this);
 	}
 	
@@ -76,13 +66,6 @@ public class ChestEspMod extends Mod implements UpdateListener, RenderListener
 				RenderUtils.blockESPBox(((EntityMinecartChest)o).getPosition());
 			}
 		}
-		for(BlockPos blockPos : matchingBlocks)
-		{
-			if(i >= maxChests)
-				break;
-			i++;
-			RenderUtils.blockESPBox(blockPos);
-		}
 		if(i >= maxChests && shouldInform)
 		{
 			wurst.chat.warning(getName() + " found §lA LOT§r of chests.");
@@ -94,35 +77,8 @@ public class ChestEspMod extends Mod implements UpdateListener, RenderListener
 	}
 	
 	@Override
-	public void onUpdate()
-	{
-		updateMS();
-		if(hasTimePassedM(3000))
-		{
-			matchingBlocks.clear();
-			for(int y = range; y >= -range; y--)
-				for(int x = range; x >= -range; x--)
-					for(int z = range; z >= -range; z--)
-					{
-						int posX = (int)(mc.thePlayer.posX + x);
-						int posY = (int)(mc.thePlayer.posY + y);
-						int posZ = (int)(mc.thePlayer.posZ + z);
-						BlockPos pos = new BlockPos(posX, posY, posZ);
-						IBlockState state = mc.theWorld.getBlockState(pos);
-						Block block = state.getBlock();
-						int metadata = block.getMetaFromState(state);
-						if(Block.getIdFromBlock(block) == 33
-							&& (metadata == 6 || metadata == 7 || metadata == 15))
-							matchingBlocks.add(pos);
-					}
-			updateLastMS();
-		}
-	}
-	
-	@Override
 	public void onDisable()
 	{
-		wurst.events.remove(UpdateListener.class, this);
 		wurst.events.remove(RenderListener.class, this);
 	}
 }
