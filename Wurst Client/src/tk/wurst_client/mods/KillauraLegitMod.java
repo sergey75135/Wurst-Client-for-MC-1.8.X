a405a4e1252bf6dda626e530f6f36531eaccfe09
Alexander01998 <contact.wurstimperium@gmail.com>
2016-05-04 21:23:52 +0200

    Remove lookChanged from KillauraLegit
    KillauraLegit doesn't use that stuff.

--- Wurst Client/src/tk/wurst_client/mods/KillauraLegitMod.java
+++ Wurst Client/src/tk/wurst_client/mods/KillauraLegitMod.java
@@ -50,12 +50,6 @@ public class KillauraLegitMod extends Mod implements UpdateListener
 	{
 		updateMS();
 		EntityLivingBase en = EntityUtils.getClosestEntity(true, true);
-		if(en == null)
-		{
-			EntityUtils.lookChanged = false;
-			return;
-		}
-		EntityUtils.lookChanged = true;
 		if(hasTimePassedS(wurst.mods.killauraMod.yesCheatSpeed))
 			if(mc.thePlayer.getDistanceToEntity(en) <= wurst.mods.killauraMod.yesCheatRange)
 			{
@@ -77,6 +71,5 @@ public class KillauraLegitMod extends Mod implements UpdateListener
 	public void onDisable()
 	{
 		wurst.events.remove(UpdateListener.class, this);
-		EntityUtils.lookChanged = false;
 	}
 }
