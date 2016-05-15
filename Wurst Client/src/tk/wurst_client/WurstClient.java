1e60e4678f2fa10ea94ad99a1052913f50e58dfb
Alexander01998 <contact.wurstimperium@gmail.com>
2016-05-15 20:07:56 +0200

    Chnage version to 2.21.2
    Damnit!

--- Wurst Client/src/tk/wurst_client/WurstClient.java
+++ Wurst Client/src/tk/wurst_client/WurstClient.java
@@ -29,7 +29,7 @@ public enum WurstClient
 {
 	INSTANCE;
 	
-	public static final String VERSION = "2.21.1";
+	public static final String VERSION = "2.21.2";
 	public boolean startupMessageDisabled = false;
 	
 	public AnalyticsManager analytics;
