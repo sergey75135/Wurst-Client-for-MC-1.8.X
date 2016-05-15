092b09c98510323113c5c41c97d735c268edf300
Alexander01998 <contact.wurstimperium@gmail.com>
2016-05-15 18:42:17 +0200

    Change version to 2.21.1

--- Wurst Client/src/tk/wurst_client/WurstClient.java
+++ Wurst Client/src/tk/wurst_client/WurstClient.java
@@ -29,7 +29,7 @@ public enum WurstClient
 {
 	INSTANCE;
 	
-	public static final String VERSION = "2.21";
+	public static final String VERSION = "2.21.1";
 	public boolean startupMessageDisabled = false;
 	
 	public AnalyticsManager analytics;
